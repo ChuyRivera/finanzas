3a7c5852e678718e159acd8eeee7f85fd9a095db
ChuyRivera <jesuslriv@gmail.com>
2020-11-02 23:18:17 -0700

    Configuración inicial de la aplición de Finanzas hasta el mumento justo antes de modificar las rutas en el achivo app.js

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+
+const app = express();
+const port = 3000;
+
+const db = require('./db');
+
+//Config express
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.set('view engine','ejs');
+app.set('view', path.join(__dirname,'views'));
+
+//Rutas
+
+//Archivos Estaticos
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(port, ()=> console.log(`El servidor ha iniciado en el puerto ${port}`))
---
+++ src/controllers/homeController.js
@@ -0,0 +1,7 @@
+const controller = {};
+
+controller.list = (req,res)=>{
+    res.send('Holamundo');
+}
+
+module.exports = controller;
